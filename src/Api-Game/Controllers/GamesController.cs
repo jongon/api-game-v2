108a4fd55a2785e7de54ce6e9292669a57b90152
Jonathan González <jon.gon1@gmail.com>
2017-02-08 11:09:35 -0300

    Adding test method

--- src/Api-Game/Controllers/GamesController.cs
+++ src/Api-Game/Controllers/GamesController.cs
@@ -34,6 +34,12 @@ namespace Api_Game.Controllers
             return await videoGames;
         }
 
+        [HttpGet("Test")]
+        public string Test()
+        {
+            return "Hola";
+        }
+
         [HttpGet("excerpt")]
         public async Task<IEnumerable<VideoGameExcerpt>> GetWithExcerpt(string term, Paging paging)
         {
